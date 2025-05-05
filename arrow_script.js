75919435f60cb7345f49e0dc34ed4f2117a9fa25
manuja <rpmanuja123@gmail.com>
2025-05-05 13:03:02 +0530

    add new one

---
+++ arrow_script.js
@@ -0,0 +1,11 @@
+function greet(name){
+    return 'Hello ' + name +'!';
+
+}
+console.log(greet('amoda'));
+
+const arrow_greet=(name)=>{
+    return 'hello ' + name +'!' ;
+}
+
+console.log(arrow_greet('manuja'));
