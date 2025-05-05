5d8ef452d2de51dd4b8a5548366c076f9bd23c0a
manuja <rpmanuja123@gmail.com>
2025-05-05 10:38:51 +0530

    yy

---
+++ script.js
@@ -0,0 +1,16 @@
+var _var = 10;
+let _let =20;
+const _const=30;
+
+
+function scopeTest(){
+    var _var2 = 100;
+    let _let2=200;
+    const _const2=300;
+
+    console.log(_var2,_let2,_const2);
+}
+
+
+
+scopeTest();
