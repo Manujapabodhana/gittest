5a370bb09e2023e31637162dd2a2a20ba2a3a593
manuja <rpmanuja123@gmail.com>
2025-05-10 20:51:22 +0530

    Update clss.js

--- clss.js
+++ clss.js
@@ -1,15 +1,15 @@
 class Rectangle {
     constructor(_width, _height, _color) {
         this.width = _width;
-        this.height = _height; // Fixed: changed _height to height
+        this.height = _height; 
         this.color = _color;
         console.log("new rectangle created");
     }
 
     getarea() {
-        return this.width * this.height; // Now this.height is defined
+        return this.width * this.height; 
     }
 }
 
 const newRectangle = new Rectangle(2, 5, 'red');
-console.log(newRectangle.getarea()); // Output: 10
+console.log(newRectangle.getarea()); 
