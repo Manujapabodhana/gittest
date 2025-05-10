2ce0618c112bc237be38fd3bbee5a35bac7bf225
manuja <rpmanuja123@gmail.com>
2025-05-10 09:28:59 +0530

    Update array_dsc.js

--- array_dsc.js
+++ array_dsc.js
@@ -10,3 +10,11 @@ const[first,second,...rest_arguments] = arr1;
 console.log(first);
 console.log(second);
 console.log(rest_arguments);
+
+const obj1={one:10,two:20,three:30,four:40,five:50};
+
+const{one,two,...resr_obj}=obj1;
+
+console.log(one);
+console.log(two);
+console.log(resr_obj);
