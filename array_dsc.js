d03bb692b56f08c51f62a4250e3c7e59349dd957
manuja <rpmanuja123@gmail.com>
2025-05-10 09:20:57 +0530

    Update array_dsc.js

--- array_dsc.js
+++ array_dsc.js
@@ -1,6 +1,12 @@
 const arr1=[1,2,3,4,5];
 
-let a = arr1[0];
-let b = arr1[1];
+//let a = arr1[0];
+//let b = arr1[1];
 
-console.log(a,b);
+//console.log(a,b);
+
+const[first,second,...rest_arguments] = arr1;
+
+console.log(first);
+console.log(second);
+console.log(rest_arguments);
