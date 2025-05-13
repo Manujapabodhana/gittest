4829db9ec3ecb7339ca677b4970d73ed8b9b0335
manuja <rpmanuja123@gmail.com>
2025-05-13 19:55:26 +0530

    gg

---
+++ new1.js
@@ -0,0 +1,17 @@
+function displayname(firstName,lastName){
+    console.log(firstName+ " "+ lastName);
+
+}
+
+displayname("manuja","pabdhana");
+
+
+function getInfo(firstName,lastName){
+
+    let output= "my name is "+ firstName+lastName;
+    return output;
+    
+}
+
+let out= getInfo("dd","udus");
+console.log(out);
